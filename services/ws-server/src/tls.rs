5c22470cece539cbf50467854379840b836b05a1
John Vandenberg <jayvdb@gmail.com>
2026-04-26 11:41:09 +0800

    dont use unmaintained rustls-pemfile

--- services/ws-server/src/tls.rs
+++ services/ws-server/src/tls.rs
@@ -1,15 +1,15 @@
 use std::path::Path;
 
+use rustls::pki_types::pem::PemObject;
+
 type CertKeyPair = (
     rustls::pki_types::CertificateDer<'static>,
     rustls::pki_types::PrivateKeyDer<'static>,
 );
 
 pub fn load_tls_certs(cert_filename: &Path, key_filename: &Path) -> CertKeyPair {
-    let cert_pem = std::fs::read(cert_filename).unwrap();
-    let key_pem = std::fs::read(key_filename).unwrap();
-    let cert_der = rustls_pemfile::certs(&mut cert_pem.as_slice()).next().unwrap().unwrap();
-    let key_der = rustls_pemfile::private_key(&mut key_pem.as_slice()).unwrap().unwrap();
+    let cert_der = rustls::pki_types::CertificateDer::from_pem_file(cert_filename).unwrap();
+    let key_der = rustls::pki_types::PrivateKeyDer::from_pem_file(key_filename).unwrap();
     (cert_der, key_der)
 }
 
